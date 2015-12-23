c43e4a8de776b379c8514609b840eb056f7a3320
Quentame <polletquentin74@me.com>
2015-12-23 01:43:26 +0100

    FINAL 1.1

--- controller/pageController.php
+++ controller/pageController.php
@@ -1,13 +1,31 @@
 <?php
 $pages = array('menu', 'game', 'rules', 'ranking');
 if (isset($_GET['page']) && $_GET['page'] != '' || isset($_GET['error'])) {
-  if (in_array($_GET['page'], $pages)) {
-    $page = $_GET['page'];
-  } else {
-    require_once('errors.php');
-    exit();
-  }
+	if (in_array($_GET['page'], $pages)) {
+		$page = $_GET['page'];
+		$page_name = getPageName($page).' | ';
+	} else {
+		require_once('errors.php');
+		exit();
+	}
 } else {
-  $page ='menu';
+	$page ='menu';
+}
+
+function getPageName($page) {
+	switch ($page) {
+		case 'game':
+			return 'Jeux';
+			break;
+		case 'rules':
+			return 'Règles';
+			break;
+		case 'ranking':
+			return 'Classement';
+			break;
+		default:
+			return '';
+			break;
+	}
 }
 ?>
--- index.php
+++ index.php
@@ -6,10 +6,10 @@ require_once('config.php');
 <html lang="<?php echo $_SESSION['lang']; ?>">
 <head>
 	<meta charset="utf-8"/>
-	<title><?php echo SITE_NAME; ?></title>
+	<title><?php echo $page_name.SITE_NAME; ?></title>
 	<meta name="robots" content="all"/>
 	<meta name="author" content="<?php echo AUTHOR; ?>"/>
-	<meta name="description" content="<?php echo SITE_NAME; ?>"/>
+	<meta name="description" content="<?php echo SITE_NAME; ?>, jouez pour sauver Funky des griffes du temps"/>
 	<meta name="keywords" content="<?php echo SITE_NAME; ?>,projet,cercle de projet,Institut G4"/>
 	<link rel="icon" type="image/png" href="./assets/img/icon180x180.png"/>
 	<link rel="apple-touch-icon-precomposed" href="./assets/img/icon180x180.png"/>
@@ -22,8 +22,16 @@ require_once('config.php');
 	<meta name="format-detection" content="telephone=yes"/>
 	<meta name="HandheldFriendly" content="true"/>
 
-	<link href="./assets/css/main.css" type="text/css" rel="stylesheet" media="all"/>
+	<meta property="og:url" content="http://polletquentin74.fr/projects/G4/FunkyTimeTraveler/"/>
+	<meta property="og:title" content="<?php echo SITE_NAME; ?>"/>
+	<meta property="og:description" content="<?php echo SITE_NAME; ?>, jouez pour sauver Funky des griffes du temps"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:image" content="http://polletquentin74.fr/projects/G4/FunkyTimeTraveler/assets/img/icon180x180.png"/>
+	<meta property="og:image:type" content="image/png"/>
+	<meta property="og:image:width" content="180"/>
+	<meta property="og:image:height" content="180"/>
 
+	<link href="./assets/css/main.css" type="text/css" rel="stylesheet" media="all"/>
 	<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	<script src="./assets/js/jQuery.js" type="text/javascript"></script>
 </head>
